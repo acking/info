bcfa5fc10728e1ab28ef3bfe53674dc532db69b9
acking <hbchenqi@126.com>
2012-08-13 23:45:17 +0800

    aa
    aa

---
+++ views/info/footer.php
@@ -0,0 +1,9 @@
+	<!-- footer Begin -->
+	<hr>
+	<footer>
+		<p>&copy; Company 2012</p>
+	</footer>
+	<!-- footer End -->
+</div>
+</body>
+</html>
---
+++ views/info/header.php
@@ -0,0 +1,39 @@
+<?php include_once '../info-config.php'; ?>
+<!DOCTYPE html>
+<html lang="zh-CN">
+<head>
+	<meta charset="utf-8">
+	<title>Acking's info-system</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="description" content="info-system">
+	<meta name="author" content="acking">
+	<!-- Le styles -->
+	<link href="<?=$path ?>/css/bootstrap.css" rel="stylesheet">
+	<style type="text/css">
+		body {
+			padding-top: 60px;
+			padding-bottom: 40px;
+		}
+	</style>
+	<link href="<?=$path ?>/css/bootstrap-responsive.css" rel="stylesheet">
+	<link href="<?=$path ?>/css/style.css" rel="stylesheet">
+	<script src="<?=$path ?>/js/jquery.js"></script>
+</head>
+<body>
+<!-- top Begin -->
+<div class="navbar navbar-fixed-top">
+	<div class="navbar-inner">
+		<div class="container">
+			<a class="brand" href="index.php">Acking's Info-system</a>
+			<div class="nav-collapse">
+				<ul class="nav">
+				<li class="active"><a href="index.php">Home</a></li>
+				<li><a href="info_list.html">Info-list</a></li>
+				<li><a href="#">About</a></li>
+				</ul>
+			</div><!--/.nav-collapse -->
+		</div>
+	</div>
+</div>
+<!-- top End -->
+<div class="container">
